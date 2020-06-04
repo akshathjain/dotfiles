b1a7e36e65b60f133c91b76b0aca27ab35283a4f
Akshath Jain <akshath.r.jain@gmail.com>
2020-06-04 17:32:49 -0400

    uncommented code

--- setup.sh
+++ setup.sh
@@ -12,42 +12,42 @@
 
 
 
-# # Step 1)
-# # Symlink all dot files to to the home directory
-# printf "\nTransferring dotfiles"
-# files=(".zshrc")
+# Step 1)
+# Symlink all dot files to to the home directory
+printf "\nTransferring dotfiles"
+files=(".zshrc")
 pwd=$(pwd)
-# for f in ${files[@]}; do
-#     ln -sf ${pwd}/${f} ${HOME}/${f}
-# done
+for f in ${files[@]}; do
+    ln -sf ${pwd}/${f} ${HOME}/${f}
+done
 
 
 
-# # Step 2)
-# # Clone and symlink the spaceship theme for zsh
-# printf "\nInstalling zsh themes"
-# rm -rf ${ZSH}/custom/themes/spaceship-prompt
-# rm -f ${ZSH}/custom/themes/spaceship.zsh-theme
-# git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH}/custom/themes/spaceship-prompt
-# ln -s ${ZSH}/custom/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH}/custom/themes/spaceship.zsh-theme
+# Step 2)
+# Clone and symlink the spaceship theme for zsh
+printf "\nInstalling zsh themes"
+rm -rf ${ZSH}/custom/themes/spaceship-prompt
+rm -f ${ZSH}/custom/themes/spaceship.zsh-theme
+git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH}/custom/themes/spaceship-prompt
+ln -s ${ZSH}/custom/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH}/custom/themes/spaceship.zsh-theme
 
 
 
-# # Step 3)
-# # Install Homebrew
-# printf "\nInstalling Homebrew\n"
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# Step 3)
+# Install Homebrew
+printf "\nInstalling Homebrew\n"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 
 
-# # Step 4)
-# # Install iTerm2
-# printf "\nInstalling iTerm2\n"
-# brew cask install iterm2
+# Step 4)
+# Install iTerm2
+printf "\nInstalling iTerm2\n"
+brew cask install iterm2
 
 
 
-# # Step 5)
-# # Specify iIterm2 to get its preferences from this directory
+# Step 5)
+# Specify iIterm2 to get its preferences from this directory
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string ${pwd}/iterm2
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
